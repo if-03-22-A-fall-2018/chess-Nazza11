ac1281df06c6ded276bd55b37b1404277903790b
Vinzent Kronreif <vini.kro@gmail.com>
2018-11-13 11:44:15 +0100

    3rd push

--- assignment/chess.cpp
+++ assignment/chess.cpp
@@ -12,7 +12,27 @@
  */
  #include "chess.h"
  #include <stdio.h>
- 
+
+  bool is_square_ok (File file, Rank rank)
+  {
+    return (file >= 'a' && file <= 'h') && (rank >= 1 && rank <= 8);
+  }
+
+  int nr (Rank rank)
+  {
+    return 0;
+  }
+
+  int nf (File file)
+  {
+    return 0;
+  }
+
+  bool is_move_from_base_line (enum PieceColor color, Rank rank)
+  {
+    return false;
+  }
+
   bool is_piece (struct ChessPiece pc, enum PieceColor color, enum PieceType type)
   {
      return (pc.color == color && pc.type == type);
@@ -25,29 +45,52 @@
        for(int a = 0; a < 8; a++)
        {
          chess_board[i][a].is_occupied = false;
+         chess_board[i][a].piece.type = NoPiece;
        }
      }
   }
 
   struct ChessSquare *get_square (ChessBoard chess_board, File file, Rank rank)
   {
-    return 0;
+
+
+    if (is_square_ok(file,rank)) {
+        return &chess_board[rank - 1][file - 'a'];
+    }
+    else{
+      return 0;
+    }
   }
 
   bool is_square_occupied (ChessBoard chess_board, File file, Rank rank)
   {
-    return chess_board[file - 'a'][rank - 1].is_occupied;
+    return chess_board[rank - 1][file - 'a'].is_occupied;
   }
 
   bool add_piece (ChessBoard chess_board, File file, Rank rank, struct ChessPiece piece)
   {
-    return false;
+    if (is_square_ok(file, rank) && (!is_square_occupied(chess_board, file, rank))) {
+      chess_board[rank -1][file - 'a'].is_occupied = true;
+      chess_board[rank -1][file - 'a'].piece = piece;
+      return true;
+    }
+    else
+    {
+      return false;
+    }
   }
 
   struct ChessPiece get_piece (ChessBoard chess_board, File file, Rank rank)
   {
-    struct ChessPiece return_value;
-    return return_value;
+    struct ChessPiece piece;
+    piece.type = NoPiece;
+
+    if (is_square_ok(file,rank) && is_square_occupied(chess_board, file, rank)) {
+      return chess_board[rank - 1][file - 'a'].piece;
+    }
+    else{
+      return piece;
+    }
   }
 
   void setup_chess_board (ChessBoard chess_board)
